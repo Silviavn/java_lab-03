1e42d50a2a94c5369d9381bd2d5a537c0d418a81
silvia <silvia.vergara@soymas.cl>
2022-05-02 12:14:23 -0400

    ampolleta funcionando

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("hola inmundo")
+var ampolleta = document.getElementById("ampolleta")
+var botonoff = document.getElementById ("apagar")
+var botonon = document.getElementById("encender")
+
+botonon.addEventListener("click",function(){
+    ampolleta.src = "https://www.w3schools.com/js/pic_bulbon.gif"
+    
+}) 
+
+botonoff.addEventListener("click",function(){
+    ampolleta.src = "https://www.w3schools.com/js/pic_bulboff.gif"
+})
